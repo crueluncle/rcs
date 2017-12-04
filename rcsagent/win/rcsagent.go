3376bced70b000e0020c4df2f997be23df66ff41
Careyzhang <378802948@qq.com>
2017-12-04 15:06:41 +0800

    upload version

---
+++ rcsagent/win/rcsagent.go
@@ -0,0 +1,131 @@
+package main
+
+//创建一个services，业务逻辑代码放到run()函数中
+//可通过test.exe install/uninstall来安装/卸载服务，安装后可通过 start stop restart命令来控制
+//直接双击则跑在前端
+//日志仅写到文件中
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"rcs/rcsagent"
+	"rcs/utils"
+	"runtime/debug"
+	"syscall"
+
+	"github.com/kardianos/service"
+)
+
+var (
+	rconT      int    //agent断开jobsvr连接后，在多长的随机时间内重连jobsvr,agent数量可能较多，随机重连避免风暴
+	jobsvrAddr string // jobsvr地址
+)
+var logf *os.File         //将start/stop/run中逻辑代码的日志记录到文件
+var logger service.Logger //服务的系统日志器(将日志写到windows系统日志中，可在eventviewer中查看，不输出console)
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	if err := s.Install(); err != nil {
+		log.Println(err)
+	}
+	go p.run()
+	return nil
+}
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+func (p *program) run() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Panic info is: ", err, string(debug.Stack()))
+		}
+	}()
+
+	var e error
+	var tc *utils.TClient
+	var agentServer utils.TFunc = rcsagent.StartRPCserver
+
+	if e, tc = utils.NewTClient(jobsvrAddr, rconT, 0, true, agentServer); tc != nil {
+		log.Fatalln(tc.Connect())
+	}
+	log.Fatalln(e)
+}
+func init() {
+	gob.Register(&rcsagent.Script_Run_Req{})
+	gob.Register(&rcsagent.File_Push_Req{})
+	gob.Register(&rcsagent.Rcs_Restart_Req{})
+	gob.Register(&rcsagent.Rcs_Stop_Req{})
+	gob.Register(&rcsagent.Rcs_Upgrade_Req{})
+	gob.Register(&rcsagent.Rcs_HeartBeat_Req{})
+	file, _ := exec.LookPath(os.Args[0])
+	//path, _ := os.Getwd()
+	path := filepath.Dir(file)
+	logfilename := filepath.Join(path, `log/rcsagent.log`)
+	logf, _ = os.OpenFile(logfilename, syscall.O_CREAT|syscall.O_RDWR|syscall.O_APPEND, 0777)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+	log.SetOutput(logf)
+	//log.SetOutput(io.MultiWriter(logf, os.Stdout))
+	log.Println("Version:", utils.Version, " BuildTime:", utils.BuildTime, " Author:", utils.Author)
+
+	inifilename := filepath.Join(path, `cfg/rcsagent.ini`)
+	defcfg := `;section Base defines some params,'SectionName' in []  must be uniq globally.
+	[BASE]
+	rconT             = 10
+	jobsvrAddr        = 127.0.0.1:9529`
+	cf := utils.HandleConfigFile(inifilename, defcfg)
+	rconT = cf.MustInt("BASE", "rconT")
+	jobsvrAddr = cf.MustValue("BASE", "jobsvrAddr")
+}
+func main() {
+	//在此处将标准log的输出定位到一个文件，应每次执行test.exe [cmd]时会重新打开文件，文件指针会重新指向文件开头，因此为保持日志连续性，在调用log的函数中需seek到文件末尾或者以追加的方式打开
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Panic info is: ", err, string(debug.Stack()))
+		}
+	}()
+	defer logf.Close()
+
+	svcConfig := &service.Config{
+		Name:        "rcsagent",
+		DisplayName: "rcsagent",
+		Description: "rcsagent service.",
+	}
+	prg := &program{}
+	s, err := service.New(prg, svcConfig) //生成service 实例，之后通过service.Control进行控制
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(os.Args) == 2 { //如果有参数则执行相关参数命令：install uninstall start stop restart
+		if os.Args[1] == "start" { //先安装service再启动
+			err = service.Control(s, "install")
+			if err != nil {
+				log.Println(err)
+			}
+			err = service.Control(s, "start")
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			err = service.Control(s, os.Args[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else { //不带参数运行(比如直接双击)则跑在前端
+		fmt.Println("Rekagent is running in foreground now,Typing command 'rcsagent.exe start' for run in background is recommended!")
+		err = s.Run()
+		if err != nil {
+			log.Println(err)
+			logger.Error(err)
+		}
+	}
+	select {}
+}
---
+++ rcsqueryapi/rcsqueryapi.go
@@ -0,0 +1,162 @@
+//redis查询api,调用方通过rid/ip来轮询task结果
+/*
+ GET   http://127.0.0.1:9999/getsfnumsfromredis?uuid=xxxx       		//查询任务成功,失败agent数量,给调用方返回json结构
+ GET   http://127.0.0.1:9999/getagentresultfromredis?uuid=xxxx&ip=yyyy      		//查询任务指定agent结果,给调用方返回json结构
+ GET   http://127.0.0.1:9999/getresultfromredis?uuid=xxxx             		//查询任务所有agent结果,给调用方返回json结构
+*/
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"rcs/utils"
+	"runtime/debug"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+var (
+	apiServer_addr, //对外提供服务需要监听的地址
+	redisconstr, //redis连接地址
+	redispass string //redis认证密码
+	redisDB, //redis DB
+	rMaxIdle, //redis连接池最大空闲连接
+	rMaxActive int //redis连接池最大连接数
+)
+
+var logfile *os.File
+var RedisClient *redis.Pool
+
+func init() {
+	var errs error
+	logfile, errs = os.OpenFile("log/rcsqueryapi.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0777)
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	//log.SetOutput(logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
+	log.Println("Version:", utils.Version, " BuildTime:", utils.BuildTime, " Author:", utils.Author)
+	defcfg := `;section Base defines some params,'SectionName' in []  must be uniq globally.
+[BASE]
+apiServer_addr = 0.0.0.0:9528
+redisconstr = 127.0.0.1:6379
+redisDB     = 0
+redispass   = yourPassword
+rMaxIdle    = 100
+rMaxActive  = 20000`
+
+	cf := utils.HandleConfigFile("cfg/rcsqueryapi.ini", defcfg)
+	apiServer_addr = cf.MustValue("BASE", "apiServer_addr")
+	redisconstr = cf.MustValue("BASE", "redisconstr")
+	redisDB = cf.MustInt("BASE", "redisDB")
+	redispass = cf.MustValue("BASE", "redispass")
+	rMaxIdle = cf.MustInt("BASE", "rMaxIdle")
+	rMaxActive = cf.MustInt("BASE", "rMaxActive")
+	RedisClient, errs = utils.Newredisclient(redisconstr, redispass, redisDB, rMaxIdle, rMaxActive)
+	if errs != nil {
+		log.Fatalln(errs)
+	}
+}
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Panic info is: ", err, string(debug.Stack()))
+			os.Exit(1)
+		}
+	}()
+	http.HandleFunc("/gettasksfnums", getsfnumsfromredis)          //给调用方返回json结构
+	http.HandleFunc("/getagentresult", getagentresultfromredis)    //给调用方返回json结构
+	http.HandleFunc("/getagentresultinsucc", getagentresultinsucc) //给调用方返回json结构
+	http.HandleFunc("/getagentresultinfail", getagentresultinfail) //给调用方返回json结构
+	http.HandleFunc("/gettaskresult", getresultfromredis)          //给调用方返回json结构
+
+	log.Println("Start rekapi...")
+	log.Println("RcshttpAPI:query ApiServer start...:", apiServer_addr)
+	log.Fatal(http.ListenAndServe(apiServer_addr, nil))
+}
+
+func getsfnumsfromredis(w http.ResponseWriter, r *http.Request) {
+	/*访问示例:http://115.182.81.164:9999/gettasksfnums?uuid=819399f5-bab5-11e6-a24a-40a8f023614c
+	返回:json串
+	*/
+	log.Println("Got request:", r.URL)
+	uuid := r.URL.Query().Get("uuid")
+	if len(uuid) != 36 {
+	}
+	rs := utils.GetSFnumsFromRedis(uuid, RedisClient.Get())
+	resp, e := json.Marshal(rs)
+	if e != nil {
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Connection", "close")
+	if _, e := w.Write(resp); e != nil {
+	}
+}
+func getagentresultfromredis(w http.ResponseWriter, r *http.Request) {
+	/*访问示例:http://115.182.81.164:9999/getagentresult?uuid=819399f5-bab5-11e6-a24a-40a8f023614c&ip=127.0.0.1
+	返回:json串
+	*/
+	log.Println("Got request:", r.URL)
+	uuid := r.URL.Query().Get("uuid")
+	ip := r.URL.Query().Get("ip")
+	rs := utils.GetAgentResultFromRedis(uuid, ip, RedisClient.Get())
+	resp, e := json.Marshal(rs)
+	if e != nil {
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Connection", "close")
+	if _, e := w.Write(resp); e != nil {
+	}
+}
+func getagentresultinsucc(w http.ResponseWriter, r *http.Request) {
+	/*访问示例:http://115.182.81.164:9999/getagentresult?uuid=819399f5-bab5-11e6-a24a-40a8f023614c&ip=127.0.0.1
+	返回:json串
+	*/
+	log.Println("Got request:", r.URL)
+	uuid := r.URL.Query().Get("uuid")
+	ip := r.URL.Query().Get("ip")
+	rs := utils.GetAgentResultInSucc(uuid, ip, RedisClient.Get())
+	resp, e := json.Marshal(rs)
+	if e != nil {
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Connection", "close")
+	if _, e := w.Write(resp); e != nil {
+	}
+}
+func getagentresultinfail(w http.ResponseWriter, r *http.Request) {
+	/*访问示例:http://115.182.81.164:9999/getagentresult?uuid=819399f5-bab5-11e6-a24a-40a8f023614c&ip=127.0.0.1
+	返回:json串
+	*/
+	log.Println("Got request:", r.URL)
+	uuid := r.URL.Query().Get("uuid")
+	ip := r.URL.Query().Get("ip")
+	rs := utils.GetAgentResultInFail(uuid, ip, RedisClient.Get())
+	resp, e := json.Marshal(rs)
+	if e != nil {
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Connection", "close")
+	if _, e := w.Write(resp); e != nil {
+	}
+}
+func getresultfromredis(w http.ResponseWriter, r *http.Request) {
+	/*访问示例:http://115.182.81.164:9999/gettaskresult?uuid=819399f5-bab5-11e6-a24a-40a8f023614c
+	返回:json串
+	*/
+	log.Println("Got request:", r.URL)
+	uuid := r.URL.Query().Get("uuid")
+	rs := utils.GetResultFromRedis(uuid, RedisClient.Get())
+	resp, e := json.Marshal(rs)
+	if e != nil {
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Connection", "close")
+	w.Header().Set("Content-Encoding", "gzip")
+	if _, e := w.Write(resp); e != nil {
+	}
+}
